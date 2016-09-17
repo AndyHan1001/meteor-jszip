d72c791756e464d4313a01e3adcfefbffc7a2d1e
AndyHan1001 <andyhan1001@gmail.com>
2016-09-16 21:43:54 -0700

    add git for package.js

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'This is just a meteor wrapper for the npm package jszip.',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/AndyHan1001/meteor-jszip.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
